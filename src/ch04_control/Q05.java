1feed77944856bbe14ffef1841a3020530e7ea77
areumJeong <add5833@naver.com>
2024-01-17 12:46:39 +0900

    5장. 참조자료형

--- src/ch04_control/Q05.java
+++ src/ch04_control/Q05.java
@@ -3,6 +3,7 @@ package ch04_control;
 public class Q05 {
 
 	public static void main(String[] args) {
+		int count = 0;
 		for (int i = 0; i < 5; i++) {
 			for (int k = 0; k < 3; k++) {
 				if (i == 2)
