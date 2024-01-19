d2170e997fafc977430d9761486265f6bff5870b
areumJeong <add5833@naver.com>
2024-01-19 11:23:54 +0900

    9장.제어자

--- src/ch09_modifier/sec02_access/abc/A.java
+++ src/ch09_modifier/sec02_access/abc/A.java
@@ -1,5 +1,13 @@
 package ch09_modifier.sec02_access.abc;
 
 public class A {
-
+	public int a;
+	   protected int b;
+	   int c;
+	   private int d;
+	   
+	   void abc() {
+	      System.out.println(a + b + c + d);
+	      
+	   }
 }
