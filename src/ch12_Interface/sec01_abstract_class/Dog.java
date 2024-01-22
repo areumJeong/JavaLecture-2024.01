b6dbdc597bfcf1616535310f208508e57dcba161
areumJeong <add5833@naver.com>
2024-01-22 13:46:42 +0900

    12장. 인터페이스

---
+++ src/ch12_Interface/sec01_abstract_class/Dog.java
@@ -0,0 +1,10 @@
+package ch12_Interface.sec01_abstract_class;
+
+public class Dog extends Animal {
+
+	@Override
+	void cry() {
+		System.out.println("멍멍!!");
+	}
+
+}
---
+++ src/ch12_Interface/sec03_multiple/Searchable.java
@@ -0,0 +1,7 @@
+package ch12_Interface.sec03_multiple;
+
+public class Searchable {
+
+	void search(String url);
+	
+}
