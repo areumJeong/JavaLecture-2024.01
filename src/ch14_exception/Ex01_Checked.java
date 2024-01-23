8c55c71f75e8d21b27ee54b232978d495f75daec
areumJeong <add5833@naver.com>
2024-01-23 12:06:23 +0900

    인터페이스 연습

---
+++ src/ch14_exception/Ex01_Checked.java
@@ -0,0 +1,14 @@
+package ch14_exception;
+
+public class Ex01_Checked {
+
+	public static void main(String[] args)  {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Class cls - Class.forName("java.lang.Object");
+	}
+
+}
---
+++ src/ch14_exception/Ex02_Unchecked.java
@@ -0,0 +1,10 @@
+package ch14_exception;
+
+public class Ex02_Unchecked {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
